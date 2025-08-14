1c0ca27045de9481633792cb59dc13b8f93c58e2
Leyla16125 <lhuseynova16125@ada.edu.az>
2025-08-14 21:48:30 +0400

    add

---
+++ Classes_Objects/encapsulation/Program.java
@@ -0,0 +1,14 @@
+package Classes_Objects.encapsulation;
+
+public class Program {
+    public static void main(String[] args) {
+        CharacterGame character1 = new CharacterGame();
+
+        character1.setName("Sonic");
+        character1.setCurrentHealth(10);
+
+        System.out.printf("Name: %s - Health: %d\n",
+                          character1.getName(),
+                          character1.getCurrentHealth());
+    }
+}
